b500d535f97f9d68565a9d853ce662dfe8007c27
Erenakhan <mustafaakhan794@gmail.com>
2023-02-28 10:03:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,35 +2,41 @@ import React from 'react'
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [minVal,setMinVal] = useState()
-  const [maxVal,setMaxVal] = useState()
-  const [ranVal,setRanVal] = useState()
-  const generate=() =>{
-    setRanVal(Math.floor(Math.random()*(maxVal-minVal+1)+minVal))
-  }
-  return (
-    <>
-    
 
-    <h1>Number Generator</h1>
-    <div className="hero">
-      <div className="rand">
-        <div>Random Number:<span>{ranVal}</span></div>
-      </div>
-      <div className="min_max">
-        <p>Min:</p>
-        <input type="number" value={minVal} 
-        onChange={e=>setMinVal(+e.target.value)}></input>
-        <br></br>
-        <p>Max:</p>
-        <input type="number" value={maxVal} 
-        onChange={e=>setMaxVal(+e.target.value)}></input>
-      </div>
-      <div className="button">
-        <button onClick={generate}>Generate Number</button></div>  
+
+function App() {
+  const [value, setValue,] = useState(2)
+  const [password, setPassword] = useState("");
+
+  const  genPassword = () => {
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const passwordLength = value;
+    let password = "";
+  
+    for (let i = 0; i < passwordLength; i++) {
+      const randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars.substring(randomNumber, randomNumber + 1);
+    }
+     return password;
+  }
+
+      const handleClick = () => {
+        setPassword(genPassword());
+      }
+
+
+  return (
+    <div className='container'>
+    <h1>Password Generator</h1>
+      <div className='sub-container'>
+    <input className='result' value={password}/>
+        
+        <div className='length'>Password Length:<span>{value}</span></div>
+      <input type='range' className='range' max={14} value={value} onChange={((e)=>setValue(e.target.value))} />
+      <br></br>
+  <button className='generate' onClick={handleClick}>Generate</button>
+  </div>
     </div>
-    </>
   )
 }
 
